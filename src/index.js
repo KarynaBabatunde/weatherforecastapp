fe2c9a6a6a8a722f22c157ad9ee2cfb214c3d86f
KarynaBabatunde <karinabutterflyka@gmail.com>
2022-07-24 15:33:19 +0200

    upload

---
+++ src/index.js
@@ -0,0 +1,47 @@
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let dayIndex = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thuersday",
+      "Friday",
+      "Saturday"
+    ];
+    let day = days[dayIndex];
+    return day + " " + hours + ":" + minutes;
+  }
+  let dateElement = document.querySelector("#date");
+  let currentTime = new Date();
+  dateElement.innerHTML = formatDate(currentTime);
+  
+  function showWeather(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#current-temp").innerHTML = Math.round(
+      response.data.main.temp
+    );
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind").innerHTML = Math.round(
+      response.data.wind.speed
+    );
+    document.querySelector("#description").innerHTML =
+      response.data.weather[0].main;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "895f6c518a59a276ebe4654bd029d1cd";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    let city = document.querySelector("#city-index").value;
+    searchCity(city);
+  }
+  let searchForm = document.querySelector("#city-search");
+  searchForm.addEventListener("submit", handleSubmit);
+  
