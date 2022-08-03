69cbe7602424233be03e29068acd9b8bd6ef4d8b
KarynaBabatunde <karinabutterflyka@gmail.com>
2022-08-03 19:21:32 +0200

    added forecast function

--- src/index.js
+++ src/index.js
@@ -24,6 +24,36 @@ function formatDate(date) {
   let currentTime = new Date();
   dateElement.innerHTML = formatDate(currentTime);
 
+  function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+  
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </span>
+            <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+    `;
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastHTML);
+  }
+
   
   function showWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
@@ -84,4 +114,5 @@ celciusLink.addEventListener("click", convertToCelsius);
 
   let searchForm = document.querySelector("#city-search");
   searchForm.addEventListener("submit", handleSubmit);
-  
+
+  displayForecast();
